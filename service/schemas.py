e7d73fd1f7ac912e60a92fc490e3a366b5f0dfe0
Imad Dar <imaddar@icloud.com>
2026-03-07 07:37:19 -0700

    schemas v1 implementation finished

--- service/schemas.py
+++ service/schemas.py
@@ -1,13 +1,21 @@
-from datetime import datetime
-from pydantic import BaseModel
+from datetime import datetime, timezone
+from pydantic import BaseModel, Field
 
 
 # pydantic class to validate our inputs
 class PredictRequest(BaseModel):
-    
-    pass
+    symbol: str
+    time: datetime = Field(default_factory = lambda: datetime.now(tz=timezone.utc))
     
 
 # class to validate our outputs
 class PredictResponse(BaseModel):
-    pass
+    risk_label: str
+    risk_score: float = Field(ge=0, le=1)
+    top_signals: list[str]
+    computed_at: datetime
+    window_start: datetime
+    window_end: datetime
+    latency_ms: float
+    model_version: str # gives model version for debugging purposes
+    
