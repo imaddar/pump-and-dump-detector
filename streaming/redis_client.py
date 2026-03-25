1f5b14052e96eb4e7fcf9161b35079c8a911d927
Imad Dar <imaddar@icloud.com>
2026-03-24 18:23:22 -0600

    redis client interface completed

--- streaming/redis_client.py
+++ streaming/redis_client.py
@@ -1,15 +1,16 @@
 import redis
 import json
+import os
 
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+r = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379, decode_responses=True)
 
 def check_redis_token(symbol: str, key_type: str):
     return bool(r.exists(f"{key_type}:{symbol}"))
 
 def get_redis_symbol(symbol: str, key_type: str):
-    
-    pass
+    data = r.get(f"{key_type}:{symbol}")
+    return json.loads(data) if data else None
     
 def set_redis_token(symbol: str, features: dict, key_type: str):
     ttl = 7200 if key_type == "pump" else 86400
-    pass
+    r.setex(f"{key_type}:{symbol}", ttl, json.dumps(features))
